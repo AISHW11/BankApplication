93024bd3985d2222b19079712f66d2a6e5f4a859
HP <HP@192.168.0.101>
2022-04-08 16:14:59 +0530

    Bank application project commit

---
+++ MoneyMoney Bank Application/src/applicationpartforbank/MMCurrentAcc.java
@@ -0,0 +1,23 @@
+package applicationpartforbank;
+
+import framework.*;
+
+public class MMCurrentAcc extends CurrentAcc{
+
+	
+	public MMCurrentAcc(int accNo, String accNm, float accBal, float creditLimit) {
+			super(accNo, accNm, accBal, creditLimit);
+			
+		}
+	
+	public void withdrew(float wd) {
+			super.withdrew(wd);
+		}
+	
+	@Override
+	public String toString() {
+		return "MMCurrentAcc [toString()=" + super.toString() + ", getClass()=" + getClass() + ", hashCode()=" + hashCode()
+				+ "]";
+	}
+
+}
